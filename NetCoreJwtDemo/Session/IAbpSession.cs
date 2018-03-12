d21fa48fc109d94e278756be2cb3175ef6ae2772
wjkang <977865769@qq.com>
2018-03-12 15:31:21 +0800

    update

---
+++ NetCoreJwtDemo/Session/IAbpSession.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreJwtDemo.Session
+{
+    public interface IAbpSession
+    {
+        long? UserId { get; }
+
+        IDisposable Use(int? tenantId, long? userId);
+    }
+}
